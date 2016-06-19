ec797ee083d47cdf5c04621f828c9a1ee5c58607
Renã Passos de Souza <ren.boxe@gmail.com>
2016-06-19 02:28:02 -0300

    aula 2.3

--- classifica_acessos.py
+++ classifica_acessos.py
@@ -5,4 +5,15 @@ x,y = carregar_acessos()
 modelo = MultinomialNB()
 modelo.fit(x, y)
 
-print(modelo.predict([[1, 0, 1], [0,1,0], [1,0,0],[1,1,0],[1,1,1]]))
+#print(modelo.predict([[1, 0, 1], [0,1,0], [1,0,0],[1,1,0],[1,1,1]]))
+
+resultado = modelo.predict(x)
+diferencas = resultado - y
+acertos = [d for d in diferencas if d == 0]
+total_de_acertos = len(acertos)
+total_de_elementos = len(x)
+
+taxa_de_acerto = 100.0 * total_de_acertos / total_de_elementos
+
+print(taxa_de_acerto)
+print(total_de_elementos)
