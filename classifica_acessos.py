f1cf3977b9c49d00052f46af781ba3f754ad08c2
Renã Passos de Souza <ren.boxe@gmail.com>
2016-06-20 23:23:45 -0300

    aula 2.6

--- classifica_acessos.py
+++ classifica_acessos.py
@@ -2,16 +2,23 @@ from dados import carregar_acessos
 from sklearn.naive_bayes import MultinomialNB
 
 x,y = carregar_acessos()
+
+treino_dados = x[:90]
+treino_marcacoes = y[:90]
+
+teste_dados = x[-9:]
+teste_marcacoes = y[-9:]
+
 modelo = MultinomialNB()
-modelo.fit(x, y)
+modelo.fit(treino_dados, treino_marcacoes)
 
 #print(modelo.predict([[1, 0, 1], [0,1,0], [1,0,0],[1,1,0],[1,1,1]]))
 
-resultado = modelo.predict(x)
-diferencas = resultado - y
+resultado = modelo.predict(teste_dados)
+diferencas = resultado - teste_marcacoes
 acertos = [d for d in diferencas if d == 0]
 total_de_acertos = len(acertos)
-total_de_elementos = len(x)
+total_de_elementos = len(teste_marcacoes)
 
 taxa_de_acerto = 100.0 * total_de_acertos / total_de_elementos
 
