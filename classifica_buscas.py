24a8a03a1522f8b5959196e5e26cb49d346da23f
Renã Passos de Souza <ren.boxe@gmail.com>
2016-07-09 22:00:35 -0300

    aula 4

--- classifica_buscas.py
+++ classifica_buscas.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 #pd = python data_analys
 import pandas as pd
+from collections import Counter
 
 #df = data_frame
 df = pd.read_csv('busca.csv')
@@ -16,13 +17,6 @@ Ydummies_df = Y_df
 X = Xdummies_df.values
 Y = Ydummies_df.values
 
-# a eficacia do algoritmo que chuta tudo 0 ou 1
-
-acerto_de_um = sum(Y)
-acerto_de_zero = len(Y) - acerto_de_um
-taxa_de_acerto_base = 100.0 * max(acerto_de_um, acerto_de_zero) / len(Y)
-
-print("Taxa de acerto base: %.2f" % taxa_de_acerto_base)
 
 porcentagem_de_treino = 0.9
 tamanho_de_treino = int(porcentagem_de_treino * len(Y))
@@ -44,12 +38,18 @@ modelo = MultinomialNB()
 modelo.fit(treino_dados, treino_marcacoes)
 
 resultado = modelo.predict(teste_dados)
-diferencas = resultado - teste_marcacoes
-acertos = [d for d in diferencas if d == 0]
-total_de_acertos = len(acertos)
+#acertos = sum(resultado == teste_marcacoes)
+#acertos = [d for d in diferencas if d == 0]
+total_de_acertos = sum(resultado == teste_marcacoes)
 total_de_elementos = len(teste_marcacoes)
 
 taxa_de_acerto = 100.0 * total_de_acertos / total_de_elementos
 
 print("Taxa de acerto do algoritmo: %.2f" % taxa_de_acerto)
-print(total_de_elementos)
+print("Total de elementos %d " % total_de_elementos)
+
+# a eficacia do algoritmo que chuta 
+# tudo um unico valor
+acerto_base = max(Counter(teste_marcacoes).itervalues())
+taxa_de_acerto_base = 100.0 * acerto_base / len(teste_marcacoes)
+print("Taxa de acerto base: %.2f" % taxa_de_acerto_base)
